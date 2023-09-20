f6c4725f5d18b0b5ce8671d7469ed18e334b9581
lab55 <ismaelsntolv@gmail.com>
2023-09-19 21:01:37 -0300

    Exercício pronto

--- src/Fila.java
+++ src/Fila.java
@@ -1,2 +1,32 @@
 public class Fila {
+
+    private static Fila instancia = null;
+
+    private Fila() {
+
+    }
+
+    public static Fila getInstance(){
+
+        if (instancia == null){
+
+            instancia = new Fila();
+
+        }
+        return instancia;
+    }
+
+    public void imprimeDocumento(){
+
+    }
+    public void removeDocumento(){
+
+    }
+    public void removeTodosDocumentos(){
+
+    }
+
+
+
+
 }
